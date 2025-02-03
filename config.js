6cda9f6234093c319188f161dc68654066edd63d
Sanu <97109955+Mrbanu@users.noreply.github.com>
2025-02-03 08:55:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -24,6 +24,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "n6QSUZ4Z#Dy7cwe_eAvXGKQ5D6bDpY3HLaKa-0z_CBiDvhCUXfpY",
-MONGODB: process.env.MONGODB || "redis-19811.c124.us-central1-1.gce.cloud.redislabs.com:19811",
+MONGODB: process.env.MONGODB || "mongodb+srv://Sonsmon290:38423Sonsmon@cluster0.fza5e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
 OWNER_NUM: process.env.OWNER_NUM || "919539189824", // Remove this number and add your number with country code
 };
